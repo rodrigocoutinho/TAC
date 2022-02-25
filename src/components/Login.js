2f8c2e15e246876bc6814509a777bcb344896f90
Rodrigo F. Coutinho <cvc.francisco@gmail.com>
2022-02-24 22:04:29 -0300

    Ajustes de nomes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Tela para realização do login
-export default function Login () {
+const Login =  () => {
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin(){
+        
+        
+    }
 
         return (
             <form>
+                <h1>PLAUS</h1>
                 <h3>Login</h3>
                 <div className="form-group">
                     <label>E-mail</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" value ={user} onChange={e => setUser(e.target.value)} className="form-control" placeholder="Enter email" />
                 </div>
                 <p />
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" value ={password} onChange={e => setPassword(e.target.value)} className="form-control" placeholder="Enter password" />
                 </div>
                 <p />
                 <div className="form-group">
@@ -25,10 +33,12 @@ export default function Login () {
                 </div>
                 <button type="submit" className="btn btn-primary btn-block">Submit</button> &nbsp;&nbsp;&nbsp;
                 
-                <button type="register" className="btn btn-primary btn-block">Register</button>
+                <button type="register" className="btn btn-primary btn-block" onClick={handleLogin}>Register</button>
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
             </form>
         );
     }
+
+    export default Login;
