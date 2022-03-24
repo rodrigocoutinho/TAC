b435bdd56bebc5c5ffa146452a809c6108a0a139
Rodrigo F. Coutinho <cvc.francisco@gmail.com>
2022-03-23 22:30:09 -0300

    ajustes de funcionamento testes iniciais

--- features/google-search.feature
+++ features/google-search.feature
@@ -3,11 +3,12 @@ Feature: Searching for vote cards app
   As an internet user
   In order to find out more about the itunes vote cards app
   I want to be able to search for information about the itunes vote cards app
-  
+ 
   Scenario: Google search for vote cards app
-    When I search Google for "itunes vote cards app"
-    Then I should see "Vote Cards" in the results
-
-  Scenario: Google search for course of life app
-    When I search Google for "CourseOf.Life"
+    When I search Google for "Pontos turisticos em goiânia"
     Then I should see some results
+
+
+  Scenario: I should see some results
+    When I type query as "Teste"
+    Then I submit
--- features/step-definitions/google-search-steps.js
+++ features/step-definitions/google-search-steps.js
@@ -1,32 +1,49 @@
 module.exports = function () {
 
-    this.When(/^I search Google for "([^"]*)"$/, function (searchQuery) {
+  this.Then(/^I should see some results$/, function () {
 
-        return helpers.loadPage('http://www.google.com').then(function() {
+      // driver wait returns a promise so return that
+      return driver.wait(until.elementsLocated(by.css('div.g')), 10000).then(function(){
+          // return the promise of an element to the following then.
+          return driver.findElements(by.css('div.g'));
+      })
+      .then(function (elements) {
 
-            // use a method on the page object which also returns a promise
-            return page.googleSearch.preformSearch(searchQuery);
-        });
+          // verify this element has children
+          expect(elements.length).to.not.equal(0);
+      });
+  });
+
+  this.When(/^I search Google for "([^"]*)"$/, function (searchQuery) {
+
+    return helpers.loadPage('http://www.google.com').then(function() {
+  
+        // use a method on the page object which also returns a promise
+        return page.googleSearch.performSearch(searchQuery);
+    })
+  });
+
+  this.When(/^I type query as "([^"]*)"$/, function (searchQuery) {
+
+    return helpers.loadPage('http://www.google.com').then(function() {
+  
+        // use a method on the page object which also returns a promise
+        return page.googleSearch.performSearch(searchQuery);
+    })
+  });
+
+  this.Then(/^I submit$/, function () {
+
+    // driver wait returns a promise so return that
+    return driver.wait(until.elementsLocated(by.css('div.g')), 10000).then(function(){
+        // return the promise of an element to the following then.
+        return driver.findElements(by.css('div.g'));
+    })
+    .then(function (elements) {
+
+        // verify this element has children
+        expect(elements.length).to.not.equal(0);
     });
-
-    this.Then(/^I should see "([^"]*)" in the results$/, function (keywords) {
-
-        // resolves if an item on the page contains text
-        return driver.wait(until.elementsLocated(by.partialLinkText(keywords)), 10000);
-    });
-
-    this.Then(/^I should see some results$/, function () {
-
-        // driver wait returns a promise so return that
-        return driver.wait(until.elementsLocated(by.css('div.g')), 10000).then(function() {
-
-            // return the promise of an element to the following then.
-            return driver.findElements(by.css('div.g'));
-        })
-        .then(function (elements) {
-
-            // verify this element has children
-            expect(elements.length).to.not.equal(0);
-        });
-    });
+});
 };
+
